6e61d69e229dbf6501dcc5d513f93ed8a711923e
Julian Bäume <julian.baeume@open-xchange.com>
2014-01-15 09:57:00 +0100

    extend specs to reflect multiple gt modules in one file
    still passing, good work, obviously ;P

--- spec/fixtures/simpleModule.js
+++ spec/fixtures/simpleModule.js
@@ -1,10 +1,16 @@
 /* ding
  * dong
  */
-define('simpleModule', ['gettext!test/mySimpleModule'], function (gt) {
+define('simpleModule', [
+    'randomModule',
+    'gettext!test/mySimpleModule',
+    'randomModule2',
+    'gettext!test/alternativeGTModule'
+], function (random, gt, random2, altGT) {
     'use strict';
 
     console.log('There should be a translated string: ', gt('translate me!'));
+    console.log('There should be a translated string with alternative gt module: ', altGT('translate me, too! Alternatively!'));
 
     return true;
 });
--- spec/plain_gt_call_spec.js
+++ spec/plain_gt_call_spec.js
@@ -8,19 +8,43 @@ chai.use(require('chai-things'));
 
 describe('extract_gt_strings', function () {
     describe('should extract plain gt calls', function () {
+        function findString(str, items) {
+            return items.filter(function (item) {
+                return item.msgid === str;
+            })[0];
+        }
         it('from simple example file', function (done) {
             expect('tmp/i18n/simple.pot').to.be.a.file().and.not.empty;
             PO.load('tmp/i18n/simple.pot', function (err, po) {
                 //msgid extracted
                 expect(po.items).to.contain.a.thing.with.property('msgid', 'translate me!');
 
-                var item = po.items.filter(function (item) {
-                    return item.msgid === 'translate me!';
-                })[0];
+                var item = findString('translate me!', po.items);
                 //file and line number extracted
-                expect(item.references).to.contain('spec/fixtures/simpleModule.js:7');
+                expect(item.references).to.contain('spec/fixtures/simpleModule.js:12');
                 //module name extracted
                 expect(item.references).to.contain('module:test/mySimpleModule');
+                expect(err).to.equal(null);
+                done();
+            });
+        });
+        it('should respect multiple gettext modules of require', function (done) {
+            PO.load('tmp/i18n/simple.pot', function (err, po) {
+                //msgid extracted
+                expect(po.items).to.contain.a.thing.with.property('msgid', 'translate me!');
+
+                var item = findString('translate me!', po.items);
+                //file and line number extracted
+                expect(item.references).to.contain('spec/fixtures/simpleModule.js:12');
+                //module name extracted
+                expect(item.references).to.contain('module:test/mySimpleModule');
+
+                item = findString('translate me, too! Alternatively!', po.items);
+                //file and line number extracted
+                expect(item.references).to.contain('spec/fixtures/simpleModule.js:13');
+                //module name extracted
+                expect(item.references).to.contain('module:test/alternativeGTModule');
+
                 expect(err).to.equal(null);
                 done();
             });
