b5d3c07f08870d52f58c45b80dfca02d5e444b96
Julian Bäume <julian@svg4all.de>
2014-06-20 19:17:20 +0200

    move more code into extract library
    simplify re-use for later features

--- tasks/create_pot.js
+++ tasks/create_pot.js
@@ -9,60 +9,16 @@
 'use strict';
 
 module.exports = function (grunt) {
-    var _ = require('lodash');
-    var extractStrings = require('../lib/extract')(grunt).extractStrings;
 
     grunt.registerMultiTask('create_pot', 'Extract calls to gettext that is used with a requirejs gettext module.', function () {
         var PO = require('pofile');
+        var extract = require('../lib/extract')(grunt);
         var options = this.options();
 
         this.files.forEach(function (file) {
-            var poItems;
-
-            poItems = file.src.filter(function (srcFile) {
+            var poItems = extract.fromFiles(file.src.filter(function (srcFile) {
                 return srcFile.substr(-3) === '.js';
-            }).map(function (srcFile) {
-                return {srcFile: srcFile, items: extractStrings(srcFile)};
-            }).reduce(function (acc, result) {
-                if (!acc) {
-                    return;
-                }
-                var items = result.items;
-                if (!items) {
-                    items = [];
-                    grunt.log.debug('No strings extracted from file ' + result.srcFile);
-                }
-                items.forEach(function (item) {
-                    var key = item.msgId;
-                    if (item.msgContext) {
-                        key += '_' + item.msgContext;
-                    }
-                    if (!acc[key]) {
-                        acc[key] = new PO.Item();
-                    }
-                    var poItem = acc[key];
-                    poItem.msgid = item.msgId;
-                    poItem.msgid_plural = item.msgIdPlural;
-                    poItem.msgctxt = item.msgContext;
-                    if (poItem.references.indexOf(item.fileName + ':' + item.line) < 0) {
-                        poItem.references.push(item.fileName + ':' + item.line);
-                    }
-                    if (poItem.references.indexOf('module:' + item.module) < 0) {
-                        poItem.references.push('module:' + item.module);
-                    }
-                    if (poItem.msgid_plural) {
-                        //FIXME: may be, this should be handled by pofile library
-                        poItem.msgstr = ['', ''];
-                    }
-                    if (item.comment) {
-                        var commentItem = PO.parse('msgid ""\nmsgstr ""\n\n' + item.comment + '\nmsgid "temp"\nmsgstr""\n').items[0];
-                        _(poItem.flags).extend(commentItem.flags);
-                        poItem.extractedComments = _.uniq([].concat(poItem.extractedComments, commentItem.extractedComments));
-                    }
-                });
-                return acc;
-            }, {});
-
+            }), {});
 
             if (poItems) {
                 var catalog = new PO();
